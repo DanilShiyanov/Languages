6c70cf52b61d6c891430082e1fd4172d12f277d6
daniil <shiyanov.danya.20120300@mail.ru>
2022-08-12 16:30:38 +0300

    Добавил задачи 3 семинара и две задачи первого дз

---
+++ Seminar3/task 18/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задача 18: Напишите программу, которая по заданному номеру четверти, 
+показывает диапазон возможных координат точек в этой четверти (x и y).
+*/
+
+void GetCoordinate (int quoter)
+{
+if (quoter == 1) 
+    {
+        Console.WriteLine("x > 0 и y > 0");
+    }
+    else if (quoter == 2) 
+    {
+        Console.WriteLine("x < 0 и y > 0");
+    }
+     else if (quoter == 3) 
+    {
+        Console.WriteLine("x < 0 и y < 0");
+    } 
+    else if (quoter == 4) 
+    {
+        Console.WriteLine("x > 0 и y < 0");
+    }
+    else 
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("ОШИБКА! Введено неверное число");
+        Console.ResetColor();
+    }
+}
+
+Console.WriteLine("Введите число от 0 до 4: ");
+int quoterNum = Convert.ToInt32(Console.ReadLine());
+GetCoordinate(quoterNum);
---
+++ Seminar3/task 21/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Напишите программу, которая принимает на вход координаты двух точек 
+и находит расстояние между ними в 2D пространстве.
+A (3,6); B (2,1) -> 5,09 
+A (7,-5); B (1,-1) -> 7,21
+*/
+
+double FindDistance (int x1, int y1, int x2, int y2);
+{
+    double CalculateX = (x2 - x1)*(x2 - x1);
+    double CalculateY = (y2 - y1)*(y2 - y1);
+result = Math.Sqrt(CalculateX + CalculateY);
+    return result;
+}
+
+Console.WriteLine("Введите X для точки А: ");
+int userAX = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите Y для точки A: ");
+int userAY = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите X для точки B: ");
+int userBX = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите Y для точки B: ");
+int userBY = Convert.ToInt32(Console.ReadLine());
+
+double Distance = FindDistance ( userAX, userAY, userBX, userBY);
+Console.WriteLine($"Расстояние равно: {Distance} ");
